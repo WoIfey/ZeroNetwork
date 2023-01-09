fb7c6b8ea7a66f78fd7ce57535b1cd0c10223f2c
Philip Huynh <87246869+WoIfey@users.noreply.github.com>
2023-01-09 23:44:21 +0200

    transfer

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ window.onscroll = function () {
 scrollTopButton.onclick = function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
+
+function initServerData(serverIp, serverPort) {
+  const serverIpElement = document.getElementById("server-ip");
+  serverIpElement.innerHTML = serverIp;
+  fetch("https://mcapi.us/server/status?ip=" + serverIp + "&port=" + serverPort)
+    .then((response) => response.json())
+    .then((data) => handleServerStatus(data));
+
+  function handleServerStatus(data) {
+    if (data.status == "error") {
+      console.log(data.error);
+      return false;
+    }
+
+    document.querySelector("#online-status").innerHTML = status.online
+      ? "offline"
+      : "online";
+
+    const playerCounter = document.getElementById("player-count");
+    playerCounter.innerHTML = data.players.now;
+
+    const logo = document.getElementById("server-icon");
+    logo.src = data.favicon;
+  }
+}
+initServerData("joe.onthewifi.com", "25565");
