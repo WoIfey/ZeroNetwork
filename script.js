09ec15173c25360b0e4320f984227341804009f9
Philip Huynh <87246869+WoIfey@users.noreply.github.com>
2023-01-09 20:04:34 +0200

    changes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const scrollTopButton = document.getElementById("scrollTopButton");
 
-// Show the button when the user scrolls down
 window.onscroll = function () {
   if (window.pageYOffset > 2300) {
     scrollTopButton.style.display = "block";
@@ -9,7 +8,6 @@ window.onscroll = function () {
   }
 };
 
-// When the user clicks the button, scroll to the top of the document
 scrollTopButton.onclick = function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
