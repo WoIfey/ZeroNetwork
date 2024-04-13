f300c0dcf52e51bc9789240be329e04b244ddaeb
Philip Huynh <wuflerz@outlook.com>
2024-04-13 04:21:18 +0300

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Server from '@/components/Server'
+import Home from '@/components/Home'
 import { getServers, getTimeline } from '@/utils/handleDatabase'
 
-export default async function Home() {
+export default async function Page() {
 	let timeline = await getTimeline()
 	let servers = await getServers()
 	timeline.sort((a: any, b: any) => b.id - a.id)
 	return (
 		<div className="bg-gray-900">
-			<Server timeline={timeline} servers={servers} />
+			<Home timeline={timeline} servers={servers} />
 		</div>
 	)
 }
