43356cf4836a42fac3f151b4bced2b63c367ad70
Philip Huynh <wuflerz@outlook.com>
2025-04-12 02:48:21 +0300

    reset migrations

--- prisma/migrations/20250220125213_subtitle/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `subtitle` to the `timeline` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "timeline" ADD COLUMN     "images" TEXT[],
-ADD COLUMN     "subtitle" TEXT NOT NULL;
--- prisma/migrations/20250225191728_alt/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "timeline" ADD COLUMN     "alt" TEXT[];
