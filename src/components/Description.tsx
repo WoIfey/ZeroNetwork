5fb3a0abdaa924cca0e7a237a73f2c8dc0d42fbb
Philip Huynh <wuflerz@outlook.com>
2025-05-01 01:54:55 +0300

    y

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -58,8 +58,8 @@ export default function Description() {
 							className={`group relative rounded-3xl p-8 transition-all duration-300
 				${
 					index === 0 || index === facts.length - 1
-						? 'lg:col-span-2 bg-gradient-to-br from-blue-500/10 to-purple-500/10 hover:from-blue-500/20 hover:to-purple-500/20'
-						: 'bg-gradient-to-br from-gray-200/20 to-gray-900/10 hover:from-gray-200/30 hover:to-gray-900/20 dark:from-gray-500/10 dark:to-gray-900/20 dark:hover:from-gray-500/20 dark:hover:to-gray-900/30'
+						? 'lg:col-span-2 bg-gradient-to-br lg:from-blue-500/10 lg:to-purple-500/10 lg:hover:from-blue-500/20 lg:hover:to-purple-500/20'
+						: 'bg-gradient-to-br from-gray-200/20 to-gray-900/10 lg:hover:from-gray-200/30 lg:hover:to-gray-900/20 dark:from-gray-500/10 dark:to-gray-900/20 lg:dark:hover:from-gray-500/20 lg:dark:hover:to-gray-900/30'
 				}`}
 							initial={{ opacity: 0, y: 20 }}
 							whileInView={{ opacity: 1, y: 0 }}
